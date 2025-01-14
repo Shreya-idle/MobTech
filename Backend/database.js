fcb27158ac0ce3629bb8ece21ff13c23d7ee1ef8
Sumit-Ks1 <sumit127624@gmail.com>
2025-01-14 12:31:43 +0530

    Updated connection string and env file

--- Backend/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://shreyaagrawwal4:5kGz5SMMbXrkp4pw@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.log(err));
